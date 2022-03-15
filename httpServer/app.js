bfc1bf4d91459071acf78595a5d76b4270e35d19
Daniel Preciado <fpreciado3@ucol.mx>
2022-03-14 22:42:13 -0600

    ended practice8

--- httpServer/app.js
+++ httpServer/app.js
@@ -6,3 +6,6 @@ http.createServer(let = (req, res) => {
     res.end('Hello world\n');
 
 }).listen(1337, '127.0.0.1');
+
+// en la sección de headers está la información detallada acerca del response y request headers
+// viene información como el tipo de conexión, tipo de contenido, etc
