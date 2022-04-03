b2fd8a75a28914af17b0a1f6daa9ce9df471ace5
Daniel Preciado <fpreciado3@ucol.mx>
2022-04-02 23:40:56 -0600

    added more files and code

--- 12. Practica 12/app.js
+++ 12. Practica 12/app.js
@@ -1,5 +1,7 @@
 var express = require('express'); // inyección de la dependencia express
+var ejs = require('ejs'); // inyección de dependencia ejs
 var app = express(); // se declara express
+app.set('view engine', 'ejs');
 
 var port = process.env.PORT || 3000 // estableciendo el puerto 3000 para listen
 app.use('/assets', express.static(__dirname + '/public')); // define como directorio virtual assets para 
@@ -12,8 +14,7 @@ app.use('/', function(req, res, next) {
 })
 
 app.get('/', function(req, res) { // handler
-    res.send(`<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head>
-    <body><h1>Hello world!</h1></body></html>`); // definiendo stylesheet
+    res.render('index'); // definiendo stylesheet/
 });
 
 app.listen(port); // activar server
@@ -25,5 +26,5 @@ app.get('/api', function(req, res) {
 
 // tercera ruta que recibe parámetro
 app.get('/person/:id', function(req, res) {
-    res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>'); // concatenación de las dos partes del html con el parámetro en params
+    res.render('person', {ID: req.params.id}); // concatenación de las dos partes del html con el parámetro en params
 });
