1728c687932f8874a8cbe76dd32f37fd149236f0
Daniel Preciado <fpreciado3@ucol.mx>
2022-04-09 21:41:23 -0500

    started practice 13

---
+++ 13. Practica 13/app.js
@@ -0,0 +1,33 @@
+var express = require('express'); // inyección de la dependencia express
+var app = express(); // se declara express
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 3003 // estableciendo el puerto 3000 para listen
+app.use('/assets', express.static(__dirname + '/public')); // define como directorio virtual assets para 
+// contenido estático dirigido a una carpeta física llamada public dentro del directorio donde está __dirname}
+// primer ruta nivel raíz
+
+app.use('/', function(req, res, next) {
+    console.log('Request Url:' + req.url);
+    next();
+})
+
+app.get('/', function(req, res) { // handler
+    res.render('index'); // definiendo stylesheet
+});
+
+app.listen(port); // activar server
+
+// segunda ruta
+app.get('/api', function(req, res) {
+    res.json({firstname: 'John', lastname: 'Doe'}); // objeto json a regresar    
+});
+
+// tercera ruta que recibe parámetro
+app.get('/person/:id', function(req, res) {
+    res.render('person', { ID: req.params.id }); // concatenación de las dos partes del html con el parámetro en params
+});
+
+app.get('/number/:id', function(req, res) {
+    res.render('number');
+});
