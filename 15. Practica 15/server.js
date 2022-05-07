b6bbfb545b67afc7f19e7335cd5c68dbb4819a61
Daniel Preciado <fpreciado3@ucol.mx>
2022-05-07 17:15:17 -0500

    finished practice 152

--- 15. Practica 15/server.js
+++ 15. Practica 15/server.js
@@ -5,25 +5,25 @@ app.use('/assets', express.static(__dirname + '/public')); //definiendo el direc
 
 app.use(express.urlencoded({extended: false})); // línea para parsear peticiones
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //definiendo ejs como motor de vistas
 
-app.get('/', (req,res) => {
+app.get('/', (req,res) => { //handler y definiendo el contenido del body del html y stylesheet
     res.send(`<!DOCTYPE html> <html lang="en"> <head><link rel="stylesheet" href="/assets/style.css"> 
     <title>Document</title> </head> 
     <body> <h1> Hola mundo </h1> 
     <body> </html>`)
 });
 
-app.get('/person/:id', (req,res) => {
+app.get('/person/:id', (req,res) => { //handler y definiendo person
     res.render('person', {Name: req.params.id, Message: req.query.message, Times: req.query.times});
 });
 
-app.get('/student', (req,res) =>{
+app.get('/student', (req,res) =>{ // handler de student
     res.render('index');
 })
 
-app.post('/student', (req,res) => {
+app.post('/student', (req,res) => { // definiendo post de student
     res.send(`First name es: ${req.body.fname}, Last name es: ${req.body.lname}`)
 })
 
-app.listen(PORT);
+app.listen(PORT); // escuchando el puerto
