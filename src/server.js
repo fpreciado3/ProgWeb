d0c6b2fc4ee5bd7c72aacc33797ab6ed71fdce77
Daniel Preciado <fpreciado3@ucol.mx>
2022-05-14 15:35:21 -0500

    almost finished practice 17

---
+++ src/server.js
@@ -0,0 +1,16 @@
+let express = require('express'); // inyección de dependencia express
+
+let app = express; // declaración de app express
+
+let personsRoute = require('./routes/person'); // definiendo router de persons
+
+app.set('view engine', 'ejs'); // estableciendo ejs como motor de vistas
+app.use(personsRoute);
+app.use('/assets', express.static(__dirname + '/public')); // carpeta de contenido estático
+
+//definiendo puerto
+let PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log('Escuchando en el puerto 3000'); // escucha del puerto
+})
