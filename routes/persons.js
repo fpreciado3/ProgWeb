9460dc7cf0708a0780bab89bde78140c89b25dde
Daniel Preciado <fpreciado3@ucol.mx>
2022-05-29 13:02:42 -0500

    finished practice 22

--- routes/persons.js
+++ routes/persons.js
@@ -18,11 +18,29 @@ router.get('/main', function(req, res) { // get de ruta
     res.render('main'); // render de vista main
 });
 
-router.get('/deletePerson/:id', function(req,res, next) { // delete person y su route handler
+router.get('/deletePerson/:id', function(req,res, next) { // delete person y su route handler   
     Person.findByIdAndRemove(req.params.id, req.body, function(err, post) {
         if(err) return next(err);
-        res.redirect('/persons');
+        res.redirect('/persons'); // redireccion al listado
     });
+});
+
+router.get('/findById/:id', function(req, res, next) { // route handler para obtener Id
+    Person.findById(req.params.id, function(err, person) {
+        if(err) return next(err);
+        res.render('personUpdate', {person}); // render en update
+    });
+});
+
+router.post('/updatePerson', function(req, res, next) { // post de update
+    Person.findByIdAndUpdate(req.body.objId, { // recibe parametros de Id
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss }, function(err, post) { 
+            if(err) return next(err);
+            res.redirect('/persons'); // redireccion a persons
+            });
 });
 
 router.post('/addPerson', function(req, res) { // realizando addperson con POST
